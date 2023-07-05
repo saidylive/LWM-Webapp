a2edc4a519d38c2a0fbe33d73166df513d4dcbac
Md. Sheikh Saidy <saidylive@gmail.com>
2023-07-05 12:24:49 +0600

    adding description

--- src/components/gallery_slider/index.js
+++ src/components/gallery_slider/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import imageDescription from "./gallery_description";
 
 import "./gallery.css";
+import imageDescriptions from './gallery_description';
 
 function GallerySlider(props) {
     const [useZoom, setUseZoom] = useState(false)
@@ -30,20 +32,33 @@ function GallerySlider(props) {
     }
 
     const getTitleName = () => {
-        let title = image_items?.length ? ` (${image_items[0].key})` : ''
+        let title = image_items?.length ? ` (${image_items[0].key})` : 'Gallery'
+        if(image_items?.length){
+            const description = Object.hasOwnProperty.call(imageDescriptions, image_items[0].key) ? imageDescriptions[image_items[0].key] : null
+            if(description){
+                title = description.name
+                if(description.location){
+                    // title += ` (${description.location})`
+                }
+            }
+        }
         return title
     }
 
     return <div className={`gallery-container ${useZoom ? 'lg' : ''}`}>
         <div className='gallery-header'>
-            <h3 className='gallery-title'>Gallery {getTitleName()}</h3>
+            <h3 className='gallery-title'>{getTitleName()}</h3>
             <div className='gallery-close' onClick={onClickGalleryClose}>&times;</div>
         </div>
         <div className='slider-container'>
             <Carousel {...sliderConfig}>
                 {image_items.map((item, index) => {
+                    const description = Object.hasOwnProperty.call(imageDescriptions, item.key) ? imageDescriptions[item.key] : null
                     return <div className="gallery-item" key={index}>
                         <img src={item.url} alt={item.key} />
+                        {description ? <>
+                            <p className="legend">{description.description}</p>
+                        </> : null }
                     </div>
                 })}
             </Carousel>
