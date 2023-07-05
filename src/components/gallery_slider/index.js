8c12a7d7b0b6d8138f5781c236ad7ac177c11e5f
Md. Sheikh Saidy <saidylive@gmail.com>
2023-07-05 12:26:20 +0600

    change unused var

--- src/components/gallery_slider/index.js
+++ src/components/gallery_slider/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import imageDescription from "./gallery_description";
 
 import "./gallery.css";
 import imageDescriptions from './gallery_description';
